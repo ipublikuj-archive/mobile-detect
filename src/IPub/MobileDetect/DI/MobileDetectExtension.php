45db234c040040efba5be159614ad325e01fd23e
Adam Kadlec <adam.kadlec@fastybird.com>
2015-01-10 18:36:22 +0100

    - Removed old version fallback

--- src/IPub/MobileDetect/DI/MobileDetectExtension.php
+++ src/IPub/MobileDetect/DI/MobileDetectExtension.php
@@ -15,24 +15,12 @@
 namespace IPub\MobileDetect\DI;
 
 use Nette;
-use Nette\DI\Compiler;
-use Nette\DI\Configurator;
+use Nette\DI;
 use Nette\PhpGenerator as Code;
 
 use IPub;
 
-if (!class_exists('Nette\DI\CompilerExtension')) {
-	class_alias('Nette\Config\CompilerExtension', 'Nette\DI\CompilerExtension');
-	class_alias('Nette\Config\Compiler', 'Nette\DI\Compiler');
-	class_alias('Nette\Config\Helpers', 'Nette\DI\Config\Helpers');
-}
-
-if (isset(Nette\Loaders\NetteLoader::getInstance()->renamed['Nette\Configurator']) || !class_exists('Nette\Configurator')) {
-	unset(Nette\Loaders\NetteLoader::getInstance()->renamed['Nette\Configurator']); // fuck you
-	class_alias('Nette\Config\Configurator', 'Nette\Configurator');
-}
-
-class MobileDetectExtension extends Nette\DI\CompilerExtension
+class MobileDetectExtension extends DI\CompilerExtension
 {
 	/**
 	 * @var array
