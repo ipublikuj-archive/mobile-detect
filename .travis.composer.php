d523f33199f5ebbff3f278996b5a1940a1679013
Adam Kadlec <adam.kadlec@fastybird.com>
2018-01-14 14:51:51 +0100

    - Updating test settings

--- .travis.composer.php
+++ .travis.composer.php
@@ -10,10 +10,7 @@ echo "Nette version " . $version . PHP_EOL;
 
 $file = __DIR__ . '/composer.json';
 $content = file_get_contents($file);
-$composer  = json_decode($content, TRUE);
-if (!isset($composer['require']['mobiledetect/mobiledetectlib'])) {
-	exit(255);
-}
+$composer = json_decode($content, TRUE);
 $composer['require']['nette/application'] = $version;
 $composer['require']['nette/di'] = $version;
 $composer['require']['nette/http'] = $version;
